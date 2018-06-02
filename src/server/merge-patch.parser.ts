bb354b3ea6bbea783d016b209df2d06d0accb8bf
Tiago Mariani Palte <tiagompalte@hotmail.com>
2018-06-02 15:58:15 -0300

    Tratamento de erros

--- src/server/merge-patch.parser.ts
+++ src/server/merge-patch.parser.ts
@@ -1,4 +1,5 @@
 import * as restify from 'restify'
+import { BadRequestError } from 'restify-errors'
 
 const mpContentType = 'application/merge-patch+json'
 
@@ -9,7 +10,7 @@ export const mergePatchBodyParser = (req: restify.Request, resp: restify.Respons
             req.body = JSON.parse(req.body)
         }
         catch(e) {
-            return next(new Error(`Invalid content: ${e.message}`))
+            return next(new BadRequestError(`Invalid content: ${e.message}`))
         }
     }
     return next()
--- src/users/users.router.ts
+++ src/users/users.router.ts
@@ -1,6 +1,7 @@
 import * as restify from 'restify'
 import {Router} from '../common/router'
 import {User} from './users.model'
+import { NotFoundError } from 'restify-errors';
 
 class UsersRouter extends Router {
 
@@ -13,16 +14,22 @@ class UsersRouter extends Router {
 
     applyRoutes(app: restify.Server) {
         app.get('/users', (req, resp, next) => {
-            User.find().then(this.render(resp,next))
+            User.find()
+                .then(this.render(resp,next))
+                .catch(next)
         })
 
         app.get('/users/:id', (req, resp, next) => {
-            User.findById(req.params.id).then(this.render(resp,next))
+            User.findById(req.params.id)
+                .then(this.render(resp,next))
+                .catch(next)
         })
 
         app.post('/users', (req, resp, next) => {
             let user = new User(req.body)            
-            user.save().then(this.render(resp,next))
+            user.save()
+                .then(this.render(resp,next))
+                .catch(next)
         })
 
         app.put('/users/:id', (req, resp, next) => {
@@ -33,26 +40,32 @@ class UsersRouter extends Router {
                         return User.findById(req.params.id)
                     }
                     else {
-                        resp.send(404)
+                        throw new NotFoundError('Documento não encontrado')
                     }
-                }).then(this.render(resp,next))
+                })
+                .then(this.render(resp,next))
+                .catch(next)
         })
 
         app.patch('/users/:id', (req, resp, next) => {
             const options = {new: true}
-            User.findByIdAndUpdate(req.params.id, req.body, options).then(this.render(resp,next))
+            User.findByIdAndUpdate(req.params.id, req.body, options)
+                .then(this.render(resp,next))
+                .catch(next)
         })
 
         app.del('/users/:id', (req, resp, next) => {
-            User.remove({_id:req.params.id}).exec().then((cmdResult:any) => {
-                if(cmdResult.result.n) {
-                    resp.send(204)
-                }
-                else {
-                    resp.send(404)
-                }
-                return next()
-            })
+            User.remove({_id:req.params.id}).exec()
+                .then((cmdResult:any) => {
+                    if(cmdResult.result.n) {
+                        resp.send(204)
+                    }
+                    else {
+                        throw new NotFoundError('Documento não encontrado')
+                    }
+                    return next()
+                })
+                .catch(next)
         })
     }
 }
